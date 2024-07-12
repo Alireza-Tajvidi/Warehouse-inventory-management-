9a4b344d38de36b7ab78907e4ac7bbfb227d9530
Alireza Tajvidi <A.tajvidi.official@gmail.com>
2024-07-12 17:02:30 +0300

    Login page desing was created

---
+++ WarehouseInventoryManager/WarehouseInventoryManager/LoginPage.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WarehouseInventoryManager
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.picbxLogo = new System.Windows.Forms.PictureBox();
+            this.lbUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lbPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lbForgotPassword = new System.Windows.Forms.Label();
+            this.lbSignUp = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picbxLogo
+            // 
+            this.picbxLogo.Image = ((System.Drawing.Image)(resources.GetObject("picbxLogo.Image")));
+            this.picbxLogo.Location = new System.Drawing.Point(247, 34);
+            this.picbxLogo.Name = "picbxLogo";
+            this.picbxLogo.Size = new System.Drawing.Size(132, 128);
+            this.picbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picbxLogo.TabIndex = 0;
+            this.picbxLogo.TabStop = false;
+            // 
+            // lbUsername
+            // 
+            this.lbUsername.AutoSize = true;
+            this.lbUsername.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername.Location = new System.Drawing.Point(59, 228);
+            this.lbUsername.Name = "lbUsername";
+            this.lbUsername.Size = new System.Drawing.Size(100, 21);
+            this.lbUsername.TabIndex = 1;
+            this.lbUsername.Text = "Kullanıcı adı:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(165, 223);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(329, 26);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // lbPassword
+            // 
+            this.lbPassword.AutoSize = true;
+            this.lbPassword.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword.Location = new System.Drawing.Point(114, 282);
+            this.lbPassword.Name = "lbPassword";
+            this.lbPassword.Size = new System.Drawing.Size(45, 21);
+            this.lbPassword.TabIndex = 1;
+            this.lbPassword.Text = "Şifre:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(165, 277);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(329, 26);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnLogin.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnLogin.Location = new System.Drawing.Point(227, 320);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(172, 37);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "Giriş yap";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lbForgotPassword
+            // 
+            this.lbForgotPassword.AutoSize = true;
+            this.lbForgotPassword.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbForgotPassword.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.lbForgotPassword.Location = new System.Drawing.Point(277, 360);
+            this.lbForgotPassword.Name = "lbForgotPassword";
+            this.lbForgotPassword.Size = new System.Drawing.Size(85, 16);
+            this.lbForgotPassword.TabIndex = 1;
+            this.lbForgotPassword.Text = "şifremi unuttum";
+            // 
+            // lbSignUp
+            // 
+            this.lbSignUp.AutoSize = true;
+            this.lbSignUp.Font = new System.Drawing.Font("Franklin Gothic Medium", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSignUp.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.lbSignUp.Location = new System.Drawing.Point(297, 376);
+            this.lbSignUp.Name = "lbSignUp";
+            this.lbSignUp.Size = new System.Drawing.Size(43, 16);
+            this.lbSignUp.TabIndex = 1;
+            this.lbSignUp.Text = "kayıt ol";
+            this.lbSignUp.Click += new System.EventHandler(this.lbSignUp_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(618, 452);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lbPassword);
+            this.Controls.Add(this.lbSignUp);
+            this.Controls.Add(this.lbForgotPassword);
+            this.Controls.Add(this.lbUsername);
+            this.Controls.Add(this.picbxLogo);
+            this.Name = "frmLogin";
+            this.Text = "Depo Envanter yönetici - Giriş";
+            ((System.ComponentModel.ISupportInitialize)(this.picbxLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picbxLogo;
+        private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lbPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lbForgotPassword;
+        private System.Windows.Forms.Label lbSignUp;
+    }
+}
+
---
+++ WarehouseInventoryManager/WarehouseInventoryManager/SignUpPage.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WarehouseInventoryManager
+{
+    partial class SignUpPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUsername_signup = new System.Windows.Forms.TextBox();
+            this.lbUsername_signup = new System.Windows.Forms.Label();
+            this.lbSignUp = new System.Windows.Forms.Label();
+            this.txtPassword_signup = new System.Windows.Forms.TextBox();
+            this.lbPassword_signup = new System.Windows.Forms.Label();
+            this.lbPasswordConfirm = new System.Windows.Forms.Label();
+            this.txtPasswordConfirm = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUsername_signup
+            // 
+            this.txtUsername_signup.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername_signup.Location = new System.Drawing.Point(155, 106);
+            this.txtUsername_signup.Name = "txtUsername_signup";
+            this.txtUsername_signup.Size = new System.Drawing.Size(329, 26);
+            this.txtUsername_signup.TabIndex = 4;
+            // 
+            // lbUsername_signup
+            // 
+            this.lbUsername_signup.AutoSize = true;
+            this.lbUsername_signup.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername_signup.Location = new System.Drawing.Point(49, 111);
+            this.lbUsername_signup.Name = "lbUsername_signup";
+            this.lbUsername_signup.Size = new System.Drawing.Size(100, 21);
+            this.lbUsername_signup.TabIndex = 3;
+            this.lbUsername_signup.Text = "Kullanıcı adı:";
+            // 
+            // lbSignUp
+            // 
+            this.lbSignUp.AutoSize = true;
+            this.lbSignUp.Font = new System.Drawing.Font("Franklin Gothic Medium", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSignUp.Location = new System.Drawing.Point(224, 47);
+            this.lbSignUp.Name = "lbSignUp";
+            this.lbSignUp.Size = new System.Drawing.Size(109, 34);
+            this.lbSignUp.TabIndex = 3;
+            this.lbSignUp.Text = "Kayıt ol!";
+            // 
+            // txtPassword_signup
+            // 
+            this.txtPassword_signup.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword_signup.Location = new System.Drawing.Point(155, 150);
+            this.txtPassword_signup.Name = "txtPassword_signup";
+            this.txtPassword_signup.Size = new System.Drawing.Size(329, 26);
+            this.txtPassword_signup.TabIndex = 6;
+            // 
+            // lbPassword_signup
+            // 
+            this.lbPassword_signup.AutoSize = true;
+            this.lbPassword_signup.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPassword_signup.Location = new System.Drawing.Point(104, 155);
+            this.lbPassword_signup.Name = "lbPassword_signup";
+            this.lbPassword_signup.Size = new System.Drawing.Size(45, 21);
+            this.lbPassword_signup.TabIndex = 5;
+            this.lbPassword_signup.Text = "Şifre:";
+            // 
+            // lbPasswordConfirm
+            // 
+            this.lbPasswordConfirm.AutoSize = true;
+            this.lbPasswordConfirm.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPasswordConfirm.Location = new System.Drawing.Point(46, 203);
+            this.lbPasswordConfirm.Name = "lbPasswordConfirm";
+            this.lbPasswordConfirm.Size = new System.Drawing.Size(103, 21);
+            this.lbPasswordConfirm.TabIndex = 5;
+            this.lbPasswordConfirm.Text = "Şifre (Tekrar):";
+            // 
+            // txtPasswordConfirm
+            // 
+            this.txtPasswordConfirm.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPasswordConfirm.Location = new System.Drawing.Point(155, 198);
+            this.txtPasswordConfirm.Name = "txtPasswordConfirm";
+            this.txtPasswordConfirm.Size = new System.Drawing.Size(329, 26);
+            this.txtPasswordConfirm.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SandyBrown;
+            this.button1.Font = new System.Drawing.Font("Franklin Gothic Medium", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(371, 241);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Hesap oluştur";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SignUpPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(524, 318);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPasswordConfirm);
+            this.Controls.Add(this.lbPasswordConfirm);
+            this.Controls.Add(this.txtPassword_signup);
+            this.Controls.Add(this.lbPassword_signup);
+            this.Controls.Add(this.txtUsername_signup);
+            this.Controls.Add(this.lbSignUp);
+            this.Controls.Add(this.lbUsername_signup);
+            this.Name = "SignUpPage";
+            this.Text = "SignUpPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUsername_signup;
+        private System.Windows.Forms.Label lbUsername_signup;
+        private System.Windows.Forms.Label lbSignUp;
+        private System.Windows.Forms.TextBox txtPassword_signup;
+        private System.Windows.Forms.Label lbPassword_signup;
+        private System.Windows.Forms.Label lbPasswordConfirm;
+        private System.Windows.Forms.TextBox txtPasswordConfirm;
+        private System.Windows.Forms.Button button1;
+    }
+}
